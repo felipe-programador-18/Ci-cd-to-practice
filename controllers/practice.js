cb777821239735cb0705349e51cfb0b1f0cb0cc1
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-06-02 22:01:14 -0300

    just testing git hub

--- controllers/practice.js
+++ controllers/practice.js
@@ -1,18 +1,18 @@
 
-const sum = (a,b) => {
-    return a *b
-}
-console.log(sum(20,300))
+    const sum = (a,b) => {
+        return a *b
+    }
+    console.log(sum(20,300))
 
 
-const split = (c,d) => {
-    return c /d
-}
-split(100,2)
+    const split = (c,d) => {
+        return c /d
+    }
+    split(100,2)
 
-const more = (f, g) => {
-    return f+g
-}
+    const more = (f, g) => {
+        return f+g
+    }
 
 
-module.exports = {sum, split, more}
+    module.exports = {sum, split, more}
