23b069bb695ab3b3092beedc6c7e0318ad6c7806
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-06-02 22:05:34 -0300

    just testing git hub

--- controllers/practice.test.js
+++ controllers/practice.test.js
@@ -1,14 +1,14 @@
-const sum = require("./practice")
+    const sum = require("./practice")
 
 
-test("to get my results about this", () => {
-    expect(sum.sum(40,20)).toBe(800)
-} )
+    test("to get my results about this", () => {
+        expect(sum.sum(40,20)).toBe(800)
+    } )
 
-test("split my results", () => {
-    expect(sum.split(10,5)).toBe(2)
-})
+    test("split my results", () => {
+        expect(sum.split(10,5)).toBe(2)
+    })
 
-test("testing my more", () => {
-    expect(sum.more(10,2)).toBe(12)
-})
+    test("testing my more", () => {
+        expect(sum.more(10,2)).toBe(12)
+    })
